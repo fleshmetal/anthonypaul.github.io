02b815eea057cb46d43c412881ca38e3a67173de
anthony <anthonypaul@nyu.edu>
2024-12-28 15:43:05 -0500

    SvelteKit push 3

---
+++ _app/immutable/entry/start.DV6VlZef.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CSKxAw95.js";export{t as start};
--- src/lib/slices/Hero/Scene.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import {Canvas} from "@threlte/core"
-    import Shapes from "./Shapes.svelte";
-
-</script>
-
-<Canvas shadows>
-    <Shapes />
-</Canvas>
